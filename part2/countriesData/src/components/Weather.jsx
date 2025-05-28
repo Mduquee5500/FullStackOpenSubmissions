14e74328731dc4613c93dc9bff9830ed253e2569
Mateo Duque <mduquee5500@gmail.com>
2025-05-27 20:00:35 -0700

    Removed API key

--- part2/countriesData/src/components/Weather.jsx
+++ part2/countriesData/src/components/Weather.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 const Weather = ({ capital }) => {
   const [weatherData, setWeatherData] = useState(null);
-  const API_KEY = "";
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
     if (!capital) return;
