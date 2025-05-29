4a88785fb8eba7786eb850b0c5d9eb5392a5cbf7
Mateo Duque <mduquee5500@gmail.com>
2025-05-28 17:20:45 -0700

    Step 6 Done

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -57,9 +57,17 @@ const generateID = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
     
-    if (!body.name) {
+    if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'content missing'
+        })
+    }
+
+    const existingPerson = persons.find(person => person.name === body.name)
+
+    if (existingPerson) {
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
 
